7e490aa0329d48dcbba2de053d016089a7688e46
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-11-24 11:05:02 +0100

    Fix

--- HoneyShop.Core/HoneyShop.Core/Models/User.cs
+++ HoneyShop.Core/HoneyShop.Core/Models/User.cs
@@ -4,12 +4,9 @@
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        
+        public string Role { get; set; }
     }
 }
