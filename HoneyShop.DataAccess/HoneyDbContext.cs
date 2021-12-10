5cf71f019a6ab96acc0b92d4a5d62da2d19b646c
Montekar <mantas.pasakarnis@gmail.com>
2021-12-10 12:15:11 +0100

    seeding

--- HoneyShop.DataAccess/HoneyDbContext.cs
+++ HoneyShop.DataAccess/HoneyDbContext.cs
@@ -10,33 +10,7 @@ namespace HoneyShop.DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 1, Name = "Honey", Description = "Bright fresh", Price = 5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 2, Name = "Honey", Description = "Bright solid", Price = 5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 3, Name = "Honey", Description = "Dark fresh", Price = 5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 4, Name = "Honey", Description = "Dark solid", Price = 5});
             
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 5, Name = "Bee Bread", Description = "Bee Bread 100g", Price = 4});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 6, Name = "Bee Bread", Description = "Bee Bread 200g", Price = 8});
-            
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 7, Name = "Soap 1", Description = "Soap with something 1", Price = 3.5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 8, Name = "Soap 2", Description = "Soap with something 2", Price = 3.5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 9, Name = "Soap 3", Description = "Soap with something 3", Price = 3.5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 10, Name = "Soap 4", Description = "Soap with something 4", Price = 3.5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 11, Name = "Soap 5", Description = "Soap with something 5", Price = 3.5});
-            modelBuilder.Entity<ProductEntity>()
-                .HasData(new ProductEntity() {Id = 12, Name = "Soap 6", Description = "Soap with something 6", Price = 3.5});
         }
         public virtual DbSet<ProductEntity> Products { get; set; }
         public virtual DbSet<CustomerDetailsEntity> CustomerDetails { get; set; }
--- HoneyShop.DataAccess/HoneyDbSeeder.cs
+++ HoneyShop.DataAccess/HoneyDbSeeder.cs
@@ -16,7 +16,6 @@ namespace HoneyShop.DataAccess
             _ctx.Database.EnsureDeleted();
             _ctx.Database.EnsureCreated();
             
-            var salt = "123#!";
             _ctx.CustomerDetails.Add(new CustomerDetailsEntity
             {
                 Id = 1,
@@ -37,7 +36,25 @@ namespace HoneyShop.DataAccess
 
         public void SeedProduction()
         {
-            //todo Add seed data to context
+            _ctx.Database.EnsureDeleted();
+            _ctx.Database.EnsureCreated();
+
+            _ctx.Products.Add(new ProductEntity() {Id = 1, Name = "Honey", Description = "Bright fresh", Price = 5});
+            _ctx.Products.Add(new ProductEntity() {Id = 2, Name = "Honey", Description = "Bright solid", Price = 5});
+            _ctx.Products.Add(new ProductEntity() {Id = 3, Name = "Honey", Description = "Dark fresh", Price = 5});
+            _ctx.Products.Add(new ProductEntity() {Id = 4, Name = "Honey", Description = "Dark solid", Price = 5});
+            
+            _ctx.Products.Add(new ProductEntity() {Id = 5, Name = "Bee Bread", Description = "Bee Bread 100g", Price = 4});
+            _ctx.Products.Add(new ProductEntity() {Id = 6, Name = "Bee Bread", Description = "Bee Bread 200g", Price = 8});
+            
+            _ctx.Products.Add(new ProductEntity() {Id = 7, Name = "Soap 1", Description = "Soap with something 1", Price = 3.5});
+            _ctx.Products.Add(new ProductEntity() {Id = 8, Name = "Soap 2", Description = "Soap with something 2", Price = 3.5});
+            _ctx.Products.Add(new ProductEntity() {Id = 9, Name = "Soap 3", Description = "Soap with something 3", Price = 3.5});
+            _ctx.Products.Add(new ProductEntity() {Id = 10, Name = "Soap 4", Description = "Soap with something 4", Price = 3.5});
+            _ctx.Products.Add(new ProductEntity() {Id = 11, Name = "Soap 5", Description = "Soap with something 5", Price = 3.5});
+            _ctx.Products.Add(new ProductEntity() {Id = 12, Name = "Soap 6", Description = "Soap with something 6", Price = 3.5});
+            
+            _ctx.SaveChanges();
         }
     }
 }
